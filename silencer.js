89722babc08fbceba03bd222ef3072cafb092d6e
Garrett Wilkin <garrett.wilkin@gmail.com>
2011-11-03 08:45:43 -0400

    Test passes, but it does nothing.

--- silencer.js
+++ silencer.js
@@ -1,13 +1,24 @@
+var util = require('util');
 var spawn = require('child_process').spawn;
+var EventEmitter = require('events').EventEmitter;
 
 function silencer(label,file) {
     this.label = label;
     this.file= file;
+    EventEmitter.call(this);
 };
+util.inherits(silencer,EventEmitter);
 exports.silencer = silencer;
 
 silencer.prototype.startStream = function() {
-   var self = this;
-   var process = spawn('sox', [self.file, ['-t', 'dat', '-c', '1']]);
-
-}
+    var self = this;
+    var process = spawn('sox', [self.file, ['-t', 'dat', '-c', '1']]);
+    process.stdout.on('data',function(data) {
+        console.log(data);
+        console.log('===');
+        //parse data for lines here
+        for (var line in parsedLines) {
+            self.emit('amplitude',timestamp,value);
+        }
+    });
+};
--- tests/silencer-tests.js
+++ tests/silencer-tests.js
@@ -1,16 +1,23 @@
 var vows = require('vows');
     assert = require('assert');
-    silencer = require('../silencer.js');
+    silencer = require('../silencer.js').silencer;
 
 var suite = vows.describe('silencer')
 .addBatch({                                        //Batch
     'Silencer': {                                  //Context
         'open file':{                              //SubContext
             topic: function () {                   //Topic
-                new silencer();
+                var silo = new silencer('test1','../moog.wav');
+                silo.startStream();
+                return silo;
             },
             'Returns stream object': function(error,stream) { //Vow
+                assert.isNull(error);
+                assert.isNotNull(stream);
+                assert.isObject(stream);
+                assert.instanceOf(stream,silencer);
             }
         }
     }
-});
+})
+.export(module);
